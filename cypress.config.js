f051b29a5fa0e7d3b22d4049686392a200500b8b
adebola <89344920+waximum@users.noreply.github.com>
2025-04-19 23:02:58 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  video:true,
-  
+  video: true,
+
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports/mochawesome',
+    overwrite: false,
+    html: false,
+    json: true
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      
-      
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     experimentalStudio: true,
-    baseUrl:"https://rahulshettyacademy.com/blog/index.php/2020/11/13/locators-in-selenium/"
-    
+    baseUrl: "https://rahulshettyacademy.com/blog/index.php/2020/11/13/locators-in-selenium/"
   },
-  
 });
